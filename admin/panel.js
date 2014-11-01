54ce613338f9e7c503266c6993658a38fe786936
Alex Van Camp <email@alexvan.camp>
2014-11-01 12:56:11 -0500

    Improve display of info in panel

--- admin/panel.js
+++ admin/panel.js
@@ -26,10 +26,13 @@ $(function () {
             monthusr = totals.month_top_packet.twitch_username;
         }
 
-        dayAmount.html(String(dayamt));
+        dayAmount.html(dayamt.formatMoney);
         dayUsername.html(dayusr);
-        monthAmount.html(String(monthamt));
+        dayUsername.attr('title', dayusr);
+        
+        monthAmount.html(monthamt.formatMoney);
         monthUsername.html(monthusr);
+        monthUsername.attr('title', monthusr);
     }
 
     //triggered when modal is about to be shown
@@ -58,4 +61,27 @@ $(function () {
     $('#eol-sublistener_clearall').click(function() {
        $('#eol-sublistener_list .sub').remove();
     });
+
+    Number.prototype.formatMoney = function(decPlaces, thouSeparator, decSeparator, currencySymbol) {
+        // check the args and supply defaults:
+        decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces;
+        decSeparator = decSeparator == undefined ? "." : decSeparator;
+        thouSeparator = thouSeparator == undefined ? "," : thouSeparator;
+        currencySymbol = currencySymbol == undefined ? "$" : currencySymbol;
+
+        var n = this,
+            sign = n < 0 ? "-" : "",
+            i = parseInt(n = Math.abs(+n || 0).toFixed(decPlaces)) + "",
+            j = (j = i.length) > 3 ? j % 3 : 0;
+
+        var formatted = sign + currencySymbol + (j ? i.substr(0, j) + thouSeparator : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thouSeparator) + (decPlaces ? decSeparator + Math.abs(n - i).toFixed(decPlaces).slice(2) : "");
+
+        var parts = formatted.split('.');
+        var dollars = parts[0];
+        var cents = parts[1];
+
+        return parseInt(cents) === 0
+            ? dollars
+            : formatted;
+    };
 });
