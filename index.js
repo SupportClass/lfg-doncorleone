b9c462426bd694ec80dff39105f15e33f6374f17
Alex Van Camp <email@alexvan.camp>
2014-11-12 01:05:45 -0600

    start work on extension api migration

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var io = require('../../server.js');
 var squirrel = require('squirrel');
 var events = require('events'); //needed for barry-donations event listening
 var config = require('../../lib/config');
@@ -12,46 +11,32 @@ if (!fs.existsSync(cfgPath)) {
     throw new Error('[eol-doncorleone] config.json was not present in bundles/eol-doncorleone, aborting!');
 }
 var bdConfig = JSON.parse(fs.readFileSync(cfgPath, 'utf8'));
-
 var bd = null;
-squirrel('barry-donations', function barryDonationsLoaded(err, BarryDonations) {
-    bdConfig.hostname = config.host;
-    bd = new BarryDonations(bdConfig);
-    bd.on('initialized', initialized);
-    bd.on('newdonations', gotDonations);
-});
 
-function initialized(data) {
-    console.log('[eol-doncorleone] Listening for donations to', bd.options.username);
-    cachedTotals = data.totals;
-    io.sockets.json.send({
-        bundleName: 'eol-doncorleone',
-        messageName: 'initialized',
-        content: cachedTotals
+module.exports = function(nodecg) {
+    squirrel('barry-donations', function barryDonationsLoaded(err, BarryDonations) {
+        bdConfig.hostname = config.host;
+        bd = new BarryDonations(bdConfig);
+        bd.on('initialized', initialized);
+        bd.on('newdonations', gotDonations);
     });
-}
 
-function gotDonations(data) {
-    cachedTotals = data.totals;
-    io.sockets.json.send({
-        bundleName: 'eol-doncorleone',
-        messageName: 'donations',
-        content: cachedTotals
+    function initialized(data) {
+        console.log('[eol-doncorleone] Listening for donations to', bd.options.username);
+        cachedTotals = data.totals;
+        nodecg.sendMessage('initialized', cachedTotals);
+    }
+
+    function gotDonations(data) {
+        cachedTotals = data.totals;
+        nodecg.sendMessage('donations', cachedTotals);
+    }
+
+    nodecg.listenFor('getTotals', function getTotals(data, cb) {
+        cb(cachedTotals);
     });
-}
 
-io.sockets.on('connection', function (socket) {
-    socket.on('message', function (data, fn) {
-        if (data.bundleName !== 'eol-doncorleone') {
-            return;
-        }
-
-        if (data.messageName === 'getTotals') {
-            fn(cachedTotals);
-        }
-
-        if (data.messageName === 'resetCategory') {
-            bd.resetCategory(data.content);
-        }
+    nodecg.listenFor('resetCategory', function resetCategory(data, cb) {
+        bd.resetCategory(data.content);
     });
-});
+};
