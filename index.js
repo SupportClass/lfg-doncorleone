1a311acfbe086fdbcc4b23552702edd39b12b662
Alex Van Camp <email@alexvan.camp>
2014-11-16 00:07:11 -0600

    fix infinite loopback of "initialized" events

--- index.js
+++ index.js
@@ -19,30 +19,28 @@ function DonCorleone(nodecg) {
     }
     DonCorleone.prototype._singletonInstance = this;
 
+    var self = this;
+
     squirrel('barry-donations', function barryDonationsLoaded(err, BarryDonations) {
         bdConfig.hostname = nodecg.config.host;
         bd = new BarryDonations(bdConfig);
-        bd.on('initialized', initialized);
-        bd.on('newdonations', gotDonations);
+
+        bd.on('initialized', function initialized(data) {
+            log.info('[eol-doncorleone] Listening for donations to', bd.options.username);
+            nodecg.variables.totals = data.totals;
+            nodecg.sendMessage('initialized', data);
+            self.emit('initialized', data);
+        });
+        bd.on('newdonations', function gotDonations(data) {
+            nodecg.variables.totals = data.totals;
+            nodecg.sendMessage('gotDonations', data);
+            self.emit('gotDonations', data);
+        });
     });
 
     events.EventEmitter.call(this);
 
     nodecg.declareSyncedVar({ variableName: 'totals' });
-
-    function initialized(data) {
-        log.info('[eol-doncorleone] Listening for donations to', bd.options.username);
-        nodecg.variables.totals = data.totals;
-        nodecg.sendMessage('initialized', data);
-        this.emit('initialized', data);
-    }
-
-    function gotDonations(data) {
-        nodecg.variables.totals = data.totals;
-        nodecg.sendMessage('gotDonations', data);
-        this.emit('gotDonations', data);
-    }
-
     nodecg.listenFor('resetCategory', function resetCategory(data) {
         bd.resetCategory(data);
     });
